37567eb7e52d3ddd34c1097eb9808814a868eb8e
Karagoez <aka160@post.uit.no>
2021-02-27 20:25:46 +0100

    har lagt til composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4496966af2db78cee241755aa22e341975cfd55e',
+    'name' => 'vendor_name/dte-2604-1-21v-systemutvikling',
+  ),
+  'versions' => 
+  array (
+    'vendor_name/dte-2604-1-21v-systemutvikling' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4496966af2db78cee241755aa22e341975cfd55e',
+    ),
+  ),
+);
