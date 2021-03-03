01608652a1d499f9cc483bb103454227ad825ebb
HalilKeser <hik003@post.uit.no>
2021-03-03 17:59:23 +0100

    Prøver å få twig til å fungere etter å ha separert til mapper

--- prosjektleder/index_prosjektleder.php
+++ prosjektleder/index_prosjektleder.php
@@ -1,8 +1,19 @@
 <?php
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once "../includes.php";
+
+$loader = new FilesystemLoader('prosjektleder');
+$twig = new Environment($loader);
 
 define('FILENAME_TAG', 'image');
 
-echo $twig->render('layout_admin.twig');
+try {
+    echo $twig->render('../prosjektleder/layout_prosjektleder.twig');
+} catch (\Twig\Error\LoaderError $e) {
+} catch (\Twig\Error\RuntimeError $e) {
+} catch (\Twig\Error\SyntaxError $e) {
+}
 
