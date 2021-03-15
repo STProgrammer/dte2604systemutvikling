eaa7797218c348dcaab11bb4d1479f0908ef06b5
Karagoez <aka160@post.uit.no>
2021-03-15 19:30:34 +0100

    endret User klasse

--- includes.php
+++ includes.php
@@ -1,5 +1,5 @@
 <?php
-    //phpinfo();
+
     //xdebug_info();
     $homedir = __DIR__ . '/';
 
@@ -9,6 +9,7 @@
     $rel = substr($rel, 0, -1);   // remove last "/"
     $rel = str_replace('/', '../', $rel);  //Turn "/" into "../"
     $rel = $rel . (basename($_SERVER['SCRIPT_FILENAME']) == "index.php" ? "" :"../");
+
 
     spl_autoload_register(function ($class_name) {
         $homedir = __DIR__ . '/';
@@ -26,15 +27,17 @@
     use Twig\Error\RuntimeError;
     use Twig\Error\SyntaxError;
 
+
     //HttpFoundation in $request
     $request = Request::createFromGlobals();
-
     //Session starts
     if($request->hasPreviousSession()) $session = $request->getSession();
     else $session = new Session();
     $session->start();
 
+
     error_reporting(E_ALL);
+
 
     // Twig templates
     $loader = new FilesystemLoader($homedir . 'templates');
@@ -45,10 +48,13 @@
         return XsrfProtection::getMac($action);
     }));
 
+
+
     $db = Db::getDBConnection();
     if ($db==null) {
         $session->getFlashBag()->add('header', "ERROR: Unable to connect to the database");
         echo $twig->render('error.twig', array('msg' => 'Unable to connect to the database!'));
         die();  // Abort further execution of the script
     }
+
 ?>
