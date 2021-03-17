79396a2b13cbf7d30a889f1b50cafbc8f0e868bc
Karagoez <aka160@post.uit.no>
2021-03-17 22:02:40 +0100

    edit side og funksjonalitet både for admin og selve brukeren

---
+++ user_edit.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('includes.php');
+
+
+//Denne siden er det selve innloggede brukeren skal ha tilgang til
+
+$userManager = new UserManager($db, $request, $session);
+
+
+if ($user && ($user->isAdmin() | $user->isProjectLeader())) {
+    // Change user details
+    $userToEdit = $userManager->getUser($request->query->getInt('userid'));
+    if ($request->request->has('user_edit') && XsrfProtection::verifyMac("Edit user's information")) {
+        //ONLY ADMIN CAN MAKE ANOTHER USER ADMIN
+        if (!$user->isAdmin()) {
+            $request->request->set('isAdmin', 0);
+        }
+        if ($userManager->editOtherUser($userToEdit)) {
+            header("Location: ".$request->server->get('REQUEST_URI')."&useredit=1");
+            exit();
+        } else {
+            header("Location: ".$request->server->get('REQUEST_URI')."&failedtoedituser=1");
+            exit();
+        }
+    }
+    //Delete user
+    elseif ($request->request->has('delete_user') && XsrfProtection::verifyMac("Delete user") && $user->isAdmin()) {
+        if ($userManager->deleteUser($user->getUserId())) {
+            header("Location: ".$request->server->get('REQUEST_URI')."&deleteuser=1");
+            exit();
+        } else {
+            header("Location: ".$request->server->get('REQUEST_URI')."&failedtodeleteuser=1");
+            exit();
+        }
+    }
+    else {
+        try {
+            echo $twig->render('user_edit.twig', array('session' => $session,
+                'request' => $request, 'user' => $user, 'userToEdit' => $userToEdit));
+        } catch (LoaderError | \Twig\Error\RuntimeError | \Twig\Error\SyntaxError $e) {
+
+        }
+    }
+} else {
+    header("location: login.php");
+    exit();
+}
