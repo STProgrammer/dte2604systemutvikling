9e3dcd2c013205a66da607873c8758cbd4c17752
torebjerkan <tbj034@post.uit.no>
2021-03-18 19:19:37 +0100

    Oppdaterer metode for å opprette ny gruppe. Oppretter employee_dashboard.twig og employee_dashboard.php

--- employee_dashboard.php
+++ employee_dashboard.php
@@ -7,7 +7,7 @@ define('FILENAME_TAG', 'image');
 
 if (!empty($twig)) {
     try {
-        echo $twig->render('bruker_layout.twig');
+        echo $twig->render('employee_dashboard.twig');
     } catch (LoaderError | RuntimeError | SyntaxError $e) {
     }
 }
