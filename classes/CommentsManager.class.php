ef3909a1557043fdeedbf6c07c04b093f419bcc8
Jørgen <jorgen.rypdal@gmail.com>
2021-03-19 13:19:38 +0100

    Lagt inn hour klassen, opprettet comments.php / manager klassen, kommet noe på vei med comments.twig /.php

---
+++ classes/CommentsManager.class.php
@@ -0,0 +1,39 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class CommentsManager {
+    private $db;
+    private $request;
+    private $session;
+
+    //CONSTRUCTOR//
+    function __construct(PDO $db, Request $request, Session $session) {
+        $this->db = $db;
+        $this->request = $request;
+        $this->session = $session;
+    }
+
+    // GET ALL COMMENTS
+    public function getAllHours() : array {
+        try {
+            $stmt = $this->db->prepare("SELECT * FROM Hours");
+            $stmt->execute();
+            if( $hours = $stmt->fetchAll(PDO::FETCH_CLASS, "Hour")) {
+                return $hours;
+            }
+            else {
+                $this->notifyUser("Comments not found", "Kunne ikke hente kommentarer");
+                //return new Project();
+                return array();
+            }
+        } catch (Exception $e) {
+            $this->NotifyUser("En feil oppstod, på getAllComments()", $e->getMessage());
+            print $e->getMessage() . PHP_EOL;
+            //return new Project();
+            return array();
+        }
+    }
+
+}
