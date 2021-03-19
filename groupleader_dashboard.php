8fc95ab0430c793314af99ef67fe5d5a7a8f5d8e
Jørgen <jorgen.rypdal@gmail.com>
2021-03-19 08:41:59 +0100

    Lagt inn groupleader Dash

---
+++ groupleader_dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+require_once "includes.php";
+
+define('FILENAME_TAG', 'image');
+
+
+if (!empty($twig)) {
+    try {
+        echo $twig->render('groupleader_dashboard.twig');
+    } catch (LoaderError | SyntaxError | RuntimeError $e) {
+    }
+}
