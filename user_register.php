deac3b9e037556447458fe62d887168955e873a5
Karagoez <aka160@post.uit.no>
2021-03-20 16:36:15 +0100

    fikset en feil på registreringssiden

--- user_register.php
+++ user_register.php
@@ -11,7 +11,7 @@ if ($user && ($user->isAdmin() | $user->isProjectLeader())) {
     if ($request->request->has('register') && XsrfProtection::verifyMac("user_register")) {
         //Bare admin kan registrere admin
         if ($userManager->registerUser($user)) {
-            header("Location: /userprofiles.php?registereduser=1");
+            header("Location: userprofiles.php?registereduser=1");
             exit();
         } else {
             header("Location: ?failedtoregisteruser=1");
