138251bf0d8c568f454df054a825449743bef315
Are Abrahamsen <aab057@uit.no>
2021-03-26 13:06:24 +0100

    Endringer employee_dashboard

--- employee_dashboard.php
+++ employee_dashboard.php
@@ -12,11 +12,11 @@ $hourManager = new HourManager($db, $request, $session);
 $userManager = new UserManager($db, $request, $session);
 
 if ($user) {
-    $userID = $userManager->getUserID($request->query->getInt('userID'));
-    $hour = $hourManager->getAllHoursForUser($userID, "startTime");
+    $userID = $user->getUserId($user);
+    $hour = $hourManager->getAllHoursForUser($userID, 'startTime');
 
     echo $twig->render('employee_dashboard.twig',
-        array('Hour' => $hour, 'UserID' => $userID, 'session' => $session, 'User' => $user));
+        array('Hour' => $hour, 'HourManager' => $hourManager, 'UserID' => $userID, 'session' => $session, 'User' => $user));
 } else {
     header("location: login.php");
     exit();
