3b3c2c1aa2dd5852f250bf0349e517bf87d30ec9
asbjornbjorge <abj075@post.uit.no>
2021-03-28 20:04:52 +0200

    lagt til sånn at  fungerer

--- user_contribution.php
+++ user_contribution.php
@@ -6,22 +6,21 @@ require_once('includes.php');
 //Denne siden skal bare sees av admin eller prosjektleder og viser timer brukt av en enkelt bruker på et spesifikt prosjekt
 
 $userManager = new UserManager($db, $request, $session);
-$user = $session->get('user');
-$userToView = $userManager->getUser($request->query->getInt('userid'));
+$user = $session->get('User');
 
 if ($user && ($user->isAdmin() | $user->isProjectLeader())) {
-    $userToView = $userManager->getUser($request->query->getInt('userid'));
+    $userID = $request->query->getInt('userID');
+    $userToView = $userManager->getUser($userID);
+
 
 
     try {
         echo $twig->render('user_contribution.twig', array('session' => $session,
-            'request' => $request, 'user' => $user, 'userToView' => $userToView));
+            'request' => $request, 'user' => $user, 'userToView' => $userToView, 'userID' => $userID));
     } catch (LoaderError | \Twig\Error\RuntimeError | \Twig\Error\SyntaxError $e) { echo $e->getMessage(); }
 
 
 } else {
-    try {
-        echo $twig->render('user_contribution.twig', array('session' => $session,
-            'request' => $request, 'user' => $user, 'userToView' => $userToView));
-    } catch (LoaderError | \Twig\Error\RuntimeError | \Twig\Error\SyntaxError $e) { echo $e->getMessage(); }
+    header("location: index.php");
+    exit();
 }
