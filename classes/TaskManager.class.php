b6ab0f241ff669edc1b3b95f2f3ede11d76cbf9d
Are Abrahamsen <aab057@uit.no>
2021-04-21 10:28:29 +0200

    Justerte getTasksOfUser med Nicho

--- classes/TaskManager.class.php
+++ classes/TaskManager.class.php
@@ -88,7 +88,7 @@ LEFT JOIN Tasks as parentTasks on parentTasks.taskID = Tasks.parentTask WHERE 1'
 LEFT JOIN Users as mainResponsible on mainResponsible.userID = Tasks.mainResponsible
 LEFT JOIN Tasks as parentTasks on parentTasks.taskID = Tasks.parentTask
 LEFT JOIN UsersAndGroups as groupID on groupID.groupID = Tasks.groupID 
-WHERE groupID.userID = :userID AND Tasks.hasSubtask = 0 AND Tasks.status < 3;';
+WHERE groupID.userID = :userID AND Tasks.hasSubtask >= 0 AND Tasks.status < 3;';
         try {
             $stmt = $this->db->prepare($query);
             $stmt->bindParam('userID', $userId, PDO::PARAM_INT);
