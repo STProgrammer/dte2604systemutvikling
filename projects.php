761aa7fff2468db415a6815eb0808fdce50beec4
Halil Ibrahim <hke006@uit.no>
2021-04-28 13:19:45 +0200

    Fixet noen bug

--- projects.php
+++ projects.php
@@ -9,15 +9,6 @@ if ($user) {
 
     $projectsOfUser = $ProjectManager->getProjectsOfUser($user->getUserID());
 
-    /*foreach ($projects as $project) {
-        $projectMember = $ProjectManager->getProjectMembers($project->getProjectName());
-        foreach ($projectMember as $member) {
-            if ($user->getUserId() ==  $member->getUserId()) {
-                array_push($projectsOfUser, $project);
-            }
-        }
-    }*/
-
     $userManager = new UserManager($db, $request, $session);
     $users = $userManager->getAllUsers("lastName");
 
